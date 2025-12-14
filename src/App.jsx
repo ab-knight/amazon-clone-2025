39acd9307848a4219d50bfab09022e942f0cb806
ab-knight <150024162+ab-knight@users.noreply.github.com>
2025-12-15 03:55:49 +0530

    CarouselEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Components/Header/Header'
+import CarouselEffect from './Components/Carousel/CarouselEffect';
 
 function App() {
   return (
     <>
-    <Header/>
+      <Header/>
+      <CarouselEffect/>
     </>
-  )
+  );
 }
 
 export default App
