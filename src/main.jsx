1420ba7bacfbd745082e289dc953ac12a8649d62
ab-knight <150024162+ab-knight@users.noreply.github.com>
2025-12-17 12:20:29 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import { initialState, reducer } from "./Utility/reducer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DataProvider reducer={reducer} initialState={initialState}>
-      <App />
-    </DataProvider>
+      <DataProvider reducer={reducer} initialState={initialState}>
+        <App />
+      </DataProvider>
   </React.StrictMode>
 );
