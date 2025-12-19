e2a3248c7beef245df3312af14d063508bc097cd
ab-knight <150024162+ab-knight@users.noreply.github.com>
2025-12-20 03:28:18 +0530

    Auth page UI

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,39 +1,103 @@
-import React from 'react'
-import classes from './SignUp.module.css'
-import { Link } from 'react-router-dom';
-
+import React, { useState, useContext } from "react";
+import classes from "./SignUp.module.css";
+import { Link } from "react-router-dom";
+import { auth } from "../../Utility/firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { DataContext } from "../../Components/DataProvider/DataProvider";
+import { Type } from "../../Utility/action.type"
 
 function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [{ user }, dispatch] = useContext(DataContext);
+
+  console.log(user);
+
+  const authHandler = (e) => {
+    e.preventDefault();
+
+    if (e.target.name === "signin") {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          dispatch({
+            type: Type.SET_USER,
+            user: userInfo.user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(err.message);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          dispatch({
+            type: Type.SET_USER,
+            user: userInfo.user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(err.message);
+        });
+    }
+  };
+
   return (
     <section className={classes.login}>
-      {/* logo */}
-      <Link>
-        <img src="https://pngimg.com/uploads/amazon/amazon_PNG1.png" alt="" />
+      <Link to="/">
+        <img
+          src="https://pngimg.com/uploads/amazon/amazon_PNG1.png"
+          alt="Amazon"
+        />
       </Link>
 
-      {/* form */}
       <div className={classes.login_container}>
         <h1>Sign-in</h1>
-        <form action="">
+
+        <form>
           <div>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" />
+            <label>Email</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+            />
           </div>
+
           <div>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" />
+            <label>Password</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+            />
           </div>
-          <button className={classes.login_signinButton}>sign in</button>
+
+          <button
+            name="signin"
+            type="submit"
+            onClick={authHandler}
+            className={classes.login_signinButton}
+          >
+            Sign In
+          </button>
         </form>
-        <p>
-          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
-          Sale. Please see our Privacy Notice, our Cookies Notice and our
-          Interest-Based Ads Notice.
-        </p>
-        <button className='classes.login_registerButton'>Create your Amazon Account</button>
+
+        <button
+          name="signup"
+          onClick={authHandler}
+          className={classes.login_registerButton}
+        >
+          Create your Amazon Account
+        </button>
       </div>
     </section>
   );
 }
 
-export default Auth
+export default Auth;
